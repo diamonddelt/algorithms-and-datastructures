1b16c15e3df37fe0806c48441db717c890c30902
diamonddelt <rrrasti@yahoo.com>
2020-09-16 17:25:23 -0500

    Initial commit - add reversestring implementation in js

---
+++ reversestring/javascript/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let strLength = str.length;
+    let reversedString = '';
+
+    while (strLength >= 0) {
+        reversedString += str.charAt(strLength);
+        strLength--
+    }
+
+    return reversedString;
+}
+
+module.exports = reverse;
