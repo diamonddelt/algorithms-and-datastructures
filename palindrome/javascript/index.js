071ba8d65a5a52fe11d5a62a59a0da311f7547b7
diamonddelt <rrrasti@yahoo.com>
2020-09-18 18:08:04 -0500

    Add additional solution for palindrome in js

--- palindrome/javascript/index.js
+++ palindrome/javascript/index.js
@@ -7,12 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-
 // Solution 1
 // Reverse the given input string, then compare against
 // the input string to determine if palindrome exists
 
-function palindrome(str) {
+function palindrome1(str) {
     let strLength = str.length;
     let reversedString = '';
 
@@ -28,4 +27,19 @@ function palindrome(str) {
     }
 }
 
-module.exports = palindrome;
+module.exports = palindrome1;
+
+// Solution 2
+// Use the array.every() function to check each element
+// of the string array that it matches the opposite element
+
+function palindrome2(str) {
+    let len = str.length;
+    let arr = str.split('');
+
+    return arr.every((c, i) => {
+        return c === str[len - i - 1];
+    });
+}
+
+module.exports = palindrome2;
