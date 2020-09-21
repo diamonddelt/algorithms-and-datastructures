f6df556b8e5b78f52dec639d8688e88afb00e8af
diamonddelt <rrrasti@yahoo.com>
2020-09-21 04:28:23 -0500

    Add fizzbuzz solution in js

---
+++ fizzbuzz/javascript/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+// Solution 1
+// Uses a while loop to increment up to n,
+// and determines fizz, buzz, or fizzbuzz with modulo operator
+
+function fizzBuzz(n) {
+    let count = 1;
+    while (count < n) {
+        if ((count % 3 === 0) && (count % 5 === 0)) {
+            console.log("fizzbuzz");
+        } else if (count % 3 === 0) {
+            console.log("fizz");
+        } else if (count % 5 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(count);
+        }
+        count++;
+    }
+}
+
+module.exports = fizzBuzz;
