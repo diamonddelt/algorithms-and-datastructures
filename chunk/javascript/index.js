4ccbf0086fcaca3d337fa1a69d8f299043a25a13
diamonddelt <rrrasti@yahoo.com>
2020-09-22 04:10:27 -0500

    Add chunk solution in js

---
+++ chunk/javascript/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+// Solution 1
+// Use array.slice() and iterate through array length,
+// creating subarray slices by the provided size
+
+function chunk(array, size) {
+    let chunks = [];
+    let count = 0;
+
+    while (count < array.length) {
+        chunks.push(array.slice(count, count + size));
+        count += size;
+    }
+
+    return chunks;
+}
+
+module.exports = chunk;
